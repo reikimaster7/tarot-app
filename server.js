afdc2c20351b170e33b04a972d09173c56b7cc8b
“reikimaster7git config --global user.email “worldreikimasrter@gmail.com <worldreikimaster@gmail.com  git config --global user.name reikimaster7>
2026-03-23 16:41:37 +0900

    a

--- server.js
+++ server.js
@@ -83,25 +83,25 @@ app.listen(3000, () => {
 
 let userCount = {}; // 仮（本来はDB）
 
-app.post("/api/tarot", (req, res) => {
-  const userId = req.ip;
+//app.post("/api/tarot", (req, res) => {
+//  const userId = req.ip;
 
-  if (!userCount[userId]) {
-    userCount[userId] = 0;
-  }
+ // if (!userCount[userId]) {
+ //   userCount[userId] = 0;
+ // }
 
-  if (userCount[userId] >= 3) {
-    return res.json({
-      limit: true,
-      message: "広告を見る必要があります"
-    });
-  }
+ // if (userCount[userId] >= 3) {
+ //   return res.json({
+ //     limit: true,
+ //     message: "広告を見る必要があります"
+ //   });
+ // }
 
-  userCount[userId]++;
+ // userCount[userId]++;
 
-  res.json({
-    limit: false,
-    message: "占い結果！"
-  });
-});
+  //res.json({
+  //  limit: false,
+  //  message: "占い結果！"
+  //});
+//});
 
